78f1a70e794bb63eb82e9992a44259acb2dcdfd4
derdesign <der.kunstler@gmail.com>
2012-04-10 22:14:11 -0400

    Removed symlinks from real files in docs

--- public/docs/assets/client.js
+++ public/docs/assets/client.js
@@ -1,1 +1,20 @@
-/Users/der/Node/projects/protos/resources/yuidoc-template/assets/client.js
+
+/* client.js */
+
+(function($) {
+  var codeOpen = /<pre><code>/g;
+  var codeClose = /<\/code><\/pre>/g;
+  
+  $(document).ready(function() {
+    $('h3 + .summary.description').prev().addClass('module-name');
+    var srcOUt = $('#srcOut');
+    $('div.base64-encoded').each(function() {
+      var self = $(this).removeClass('base64-encoded').addClass('base64-decoded');
+      var text = self.html();
+      var html = $.base64.decode(text).replace(codeOpen, '<pre class="brush: js">').replace(codeClose, '</pre>');
+      self.html(html);
+    });
+    SyntaxHighlighter.all();
+  });
+  
+}).call(window, jQuery);
